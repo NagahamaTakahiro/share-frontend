bbb68891595df3770cc285139ed97738acb9a1b2
NagahamaTakahiro <nnarannnara27@gmail.com>
2021-02-26 21:01:02 +0900

    message

--- src/components/HeaderAuth.vue
+++ src/components/HeaderAuth.vue
@@ -1,7 +1,7 @@
 <template>
  <div class="header flex">
    <div class="left">
-     <img class="logo" src="../assets/logo.png"/>
+     <img class="logo" src="../assets/assets/logo.png"/>
    </div>
    <div class="right flex">
      <p @click="$router.push('/signup')">新規登録</p>
